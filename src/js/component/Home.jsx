81d2fa9681fed641a9d4928c840f57eeb03e473e
Mariana Hueck <marianahueck@gmail.com>
2024-08-08 00:19:16 +0000

    cree Card

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { Card } from "./Card";
+
 
 //create your first component
 const Home = () => {
@@ -10,11 +12,11 @@ const Home = () => {
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
 			<p>
 				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
+			<Card/>
+			<Card/>
+			<Card/>
+			<Card/>
+			
 				Made by{" "}
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
 				love!
